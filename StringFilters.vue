541c6646071f06c580a7a380283e06d2fe2dfd84
Gregory Shein <13556916+kurumbus@users.noreply.github.com>
2018-08-14 23:58:00 +0300

    Create StringFilters.vue

---
+++ StringFilters.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+      filters: {
+        capitalize: function (value) {
+          if (!value) return '';
+          value = value.toString();
+          return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        wordCase: function (value) {
+            let result = value.replace( /([A-Z])/g, " $1" );
+            result = result.replace(/[_\.-]/g, ' ');
+            let finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+            return finalResult;
+        },
+        initials: function (value) {
+            let initials = value.replace(/[^a-zA-Z- ]/g, "").match(/\b\w/g);
+            return initials.join('');
+        }
+      }
+    }
+</script>
